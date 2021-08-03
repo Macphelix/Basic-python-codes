068f710a04c6ae97f178c3ed40bf1b090ba52df4
Macphelix <84187639+Macphelix@users.noreply.github.com>
2021-08-03 22:25:52 +0100

    new lines of code added to car game file

--- Python-codes/Car game.py
+++ Python-codes/Car game.py
@@ -1,10 +1,18 @@
-command = ''
+started = False
 while True:
     command = input('>').lower()
     if command == 'start':
-        print('car has started')
+        if started:
+            print('Car has already started')
+        else:
+            started = True
+            print('car has started')
     elif command == 'stop':
-        print('car has stopped')
+        if not started:
+            print('car is already stopped')
+        else:
+            started = False
+            print('car has stopped')
     elif command == 'help':
         print('''
 start - to start the car
